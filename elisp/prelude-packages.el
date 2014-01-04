e20b9b8f314acd4fd0f94678eb0deb12b1533183
George Balatsouras <gbalats@gmail.com>
2014-01-04 22:34:10 +0200

    Removing buggy emacs extensions.

--- elisp/prelude-packages.el
+++ elisp/prelude-packages.el
@@ -3,8 +3,7 @@
            gist groovy-mode haskell-mode magit
            markdown-mode paredit projectile python
            rainbow-mode solarized-theme volatile-highlights
-           yaml-mode yari zenburn-theme auto-complete
-           ac-dabbrev)
+           yaml-mode yari auto-complete ac-dabbrev)
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
