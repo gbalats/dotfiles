1b4a5b24d8b457d87dc72e7c9af2e516c694c5fb
George Balatsouras <gbalats@gmail.com>
2015-02-05 02:39:42 +0200

    Add common lb options as comments.

--- lb/setup-env.sh
+++ lb/setup-env.sh
@@ -7,6 +7,8 @@ echo >&2 "Initializing personalized LogicBlox environment ..."
 
 export LB_DEPLOYMENT_HOME="$HOME/.lb_deployment"
 export LB_CONNECTBLOX_ENABLE_ADMIN=1
+# export LB_PAGER_FORCE_START=true
+# export LB_MEM_NOWARN=true
 
 LB_LIBRARY_HOME=/opt/lb/lib
 LB_ORT_HOME=/opt/lb/default
